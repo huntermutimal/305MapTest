320ffc21bb59673f7aa2deb4937e725ef92facf2
huntermutimal <117750256+huntermutimal@users.noreply.github.com>
2024-03-13 12:18:33 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright 2019 Esri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.mycompany.app;
+
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
+import com.esri.arcgisruntime.mapping.BasemapStyle;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Viewpoint;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private MapView mapView;
+
+    public static void main(String[] args) {
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+
+        stage.setTitle("Hello World!");
+
+
+        TabPane root = new TabPane();
+        VBox vBox = new VBox();
+
+        StackPane stackPane = new StackPane();
+        Scene mapScene = new Scene(stackPane);
+        // Note: it is not best practice to store API keys in source code.
+        // An API key is required to enable access to services, web maps, and web scenes hosted in ArcGIS Online.
+        // If you haven't already, go to your developer dashboard to get your API key.
+        // Please refer to https://developers.arcgis.com/java/get-started/ for more information
+        String yourApiKey = "AAPKcf22eb129c224bb3bcf55014b784da1fvhWq8R48qV0B_h9dyoMOy0fHMJoqSE2f12Lxud4ty8-Fzm8WjkoTFpNb1wek2rJS";
+        ArcGISRuntimeEnvironment.setApiKey(yourApiKey);
+
+        // create a MapView to display the map and add it to the stack pane
+        mapView = new MapView();
+        stackPane.getChildren().add(mapView);
+
+        // create an ArcGISMap with an imagery basemap
+        ArcGISMap map = new ArcGISMap(BasemapStyle.ARCGIS_COMMUNITY);
+
+        // display the map and set a default location
+        mapView.setMap(map);
+        mapView.setViewpoint(new Viewpoint(53.5409, -113.5084, 144447.638572));
+
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/reports.fxml")); // Make sure to have / in front of file name
+        Scene reportsScene = new Scene(fxmlLoader.load());
+        Tab tab1 = new Tab("Map", mapScene.getRoot());
+        Tab tab2 = new Tab("Reports"  , reportsScene.getRoot());
+        Tab tab3 = new Tab("Settings" , new Label("Show all boats available"));
+
+        root.getTabs().add(tab1);
+        root.getTabs().add(tab2);
+        root.getTabs().add(tab3);
+
+
+        stage.setScene(new Scene(root, 300, 250));
+        stage.show();
+
+
+    }
+
+    /**
+     * Stops and releases all resources used in application.
+     */
+    @Override
+    public void stop() {
+
+        if (mapView != null) {
+            mapView.dispose();
+        }
+    }
+}
